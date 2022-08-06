143b8f03074301770edbf8325e9fcab68cf638de
Kay Kaprolat <kakaoh6@gmail.com>
2022-08-06 16:18:46 +0000

    removed remnants of repository from entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
-
-if ! (aur repo > /dev/null 2>&1); then
-    printf "AUR repo not initialized, initializing /repo...\n"
-    repo-add /repo/custom.db.tar.gz
-fi
-
 sudo pacman --noconfirm -Syu
 exec "$@"
