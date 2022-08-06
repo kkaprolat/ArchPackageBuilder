6202f9c4847398b6fff89a17ebbb1b7a4da3ac5f
Kay Kaprolat <kakaoh6@gmail.com>
2022-08-06 18:59:47 +0000

    use tarball for download instead of git

--- build_package.py
+++ build_package.py
@@ -24,9 +24,19 @@ pull_request_template = {"targetProjectId": project_id,
 	"asigneeIds": [reviewer_id]}
 
 
-os.system(f'sudo git clone https://aur.archlinux.org/{package}.git {package}_tmp')
+# backup old package files so new ones don't overwrite
 os.system(f'sudo mkdir -p {package}')
-if os.waitstatus_to_exitcode(os.system(f'diff -qrN {package} {package}_tmp')) == 0:
+os.system(f'sudo mv {package} {package}_old')
+
+# download new package files
+os.system(f'sudo wget https://aur.archlinux.org/cgit/aur.git/snapshot/{package}.tar.gz')
+os.system(f'sudo tar -xvf {package}.tar.gz')
+
+# move new files to {package}_tmp and reset name of old files
+os.system(f'sudo mv {package} {package}_tmp')
+os.system(f'sudo mv {package}_old {package}')
+
+if os.waitstatus_to_exitcode(os.system(f'diff -qrN {package} {package}_tmp')) != 0:
 	os.system(f'sudo git switch -c {package}')
 	os.system(f'sudo rm -rf {package}')
 	os.system(f'sudo mv {package}_tmp {package}')
