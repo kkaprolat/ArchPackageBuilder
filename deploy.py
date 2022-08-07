ec07d0658b19ba07fd1839e7f242849dabed31ce
Kay Kaprolat <Frank_Kay.Kaprolat@mailbox.tu-dresden.de>
2022-08-07 17:32:08 +0200

    expand ~ in paths

--- deploy.py
+++ deploy.py
@@ -12,9 +12,9 @@ project_id = 1
 reviewer_id = 1
 pull_endpoint = f"https://git.aurum.lan/api/pull-requests/{pull_id}"
 
-with open('~/.ssh/id_ed25519', 'w') as keyfile:
+with open(os.path.expanduser('~/.ssh/id_ed25519'), 'w') as keyfile:
     keyfile.write(ssh_key)
-os.chmod('~/.ssh/id_ed25519', 0o600)
+os.chmod(os.path.expanduser('~/.ssh/id_ed25519'), 0o600)
 
 r = requests.get(pull_endpoint, auth=('kay', git_pass))
 project = r.json()['sourceBranch']
